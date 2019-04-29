696557d53051511d927e7ce60209e25e4da65064
William Siegel <whsiegel@gmail.com>
2019-04-29 16:15:39 -0500

    set up main file

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+    </body>
+</html>
+
+"""
+
+
+@app.route("/")
+def index():
+    return form
